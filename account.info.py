7be43dda8f0f9d1124d7b0341bf10f9fa20b0ed1
weilinglindachen <79507011+weilinglindachen@users.noreply.github.com>
2021-08-13 15:32:09 +0800

    init commit

---
+++ account.info.py
@@ -0,0 +1,7 @@
+{
+    "username":"bullfrogchen0318@gmail.com",
+    "apikey":"&2krP$KPO2QYOx7Om@iAI5HcUGV+x$9",
+    "loki_key": "G3W!2c9Y@YNWKQ7EwLzSG^E+vG7px!v",
+    "discord_token": <discord_token>
+    
+}
